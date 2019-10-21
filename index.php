f43eafd422a8e30dcefb9776b61a61cb8f3632ac
eamirakulov <emil@amirakulov.com>
2019-10-21 17:26:22 +0600

    Return url 2

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ $checkout = new \SquareConnect\Model\CreateCheckoutRequest();
 
 $checkout->setIdempotencyKey(uniqid()); //uniqid() generates a random string.
 $checkout->setOrder($order); //this is the order we created in the previous step.
-$checkout->setRedirectUrl($_POST['return_url']); //Replace with the URL where you want to redirect your customers after transaction. 
+$checkout->setRedirectUrl('https://radicalrootsherbs.com/pages/thank-you'); //Replace with the URL where you want to redirect your customers after transaction. 
 
 //////
 try {
